bd961df686fc6ef9da9c3ffe9233a354326db9ad
santirar <santiplantielamela720@gmail.com>
2024-08-19 22:37:19 -0300

    1er commit

---
+++ ejercicio.js
@@ -0,0 +1,100 @@
+
+
+
+
+// PUNTO 1 
+
+// function parOImpar (numero1 , numero2) {
+//     if ((numero1 % 2 == 0)) {
+//       console.log("el numero es par") 
+//     } 
+//     else {
+//       console.log("el numero es impar")
+//     }
+//     if ((numero2 % 2 == 0)) {
+//       console.log("el numero es par") 
+//     }
+//     else {
+//       console.log("el numero es impar")
+//     }
+// }
+
+// parOImpar (2 , 7)
+
+
+// PUNTO 2
+
+// function mayorOIgual (numero1 , numero2) {
+//   if (numero1 > numero2) {
+//     console.log("el numero1 es mayor")
+//   }
+//   else if (numero2 > numero1) {
+//     console.log ("el numero2 es mayor")
+//   }
+//   else {
+//     console.log("los numeros son iguales")
+//   }
+// }
+
+// mayorOIgual (5 , 6)
+
+
+// PUNTO 3
+
+// function multiploDeCinco (numero1 , numero2) {
+//   if ((numero1 % 5 == 0)) {
+//     console.log("el numero1 es multiplo de 5")
+//   }
+//   else {
+//     console.log("el numero1 no es multiplo de 5")
+//   }
+//   if ((numero2 % 5 == 0)) {
+//     console.log("el numero2 no es multiplo de 5")
+//   }
+//   else {
+//     console.log("el numero2 no es multiplo de 5")
+//   }
+// }
+
+// multiploDeCinco (10 , 7)
+
+
+// PUNTO 4 
+
+// function todosLosMenoresA (numero) {
+//   for(let i = 0; i <= numero; i++) {
+//     if (i === numero + 1) break;
+//       console.log(i)
+//   }
+// }
+
+// todosLosMenoresA (10)
+
+
+// PUNTO 5
+
+// function palabraRepetida (nombre , numero){
+//   for(let i = 0; i <= nombre; i++) {
+//     if (i === nombre + 1) break;
+//     console.log(${nombre})
+//   }
+// }
+
+// let nombre = `Pepe`
+// palabraRepetida (7 , 5)
+
+
+// PUNTO 6
+
+// const valores = [
+//   "1",
+//   "2",
+//   "3",
+//   "4"
+// ]
+// function arrayValores (valores1 , valores2){
+//   for (let nombre of valores)
+//     console.log(nombre)
+// }
+
+// arrayValores (1)
